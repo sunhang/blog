2f3b8996ca823a03e022574470ca0768065b216b
sunhang <sunhang09@gmail.com>
2020-09-02 23:14:18 +0800

    兼容hexo生成的md文件

--- src/main/java/sunhang/blog/ArticleServlet.java
+++ src/main/java/sunhang/blog/ArticleServlet.java
@@ -28,9 +28,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -57,21 +55,7 @@ public class ArticleServlet extends HttpServlet {
     }
 
     private String convert(final File file) {
-        String content = null;
-        // 从文件中读取markdown内容
-        try ( BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"))) {
-            String line;
-            StringBuilder sb = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-                sb.append("\n");
-            }
-            sb.deleteCharAt(sb.length() - 1);
-            content = sb.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.toString();
-        }
+        final String content = readFile(file);
 
         // markdown to image
         try {
@@ -80,13 +64,17 @@ public class ArticleServlet extends HttpServlet {
             options.set(Parser.EXTENSIONS, Arrays.asList(new Extension[]{TablesExtension.create()}));
             Parser parser = Parser.builder(options).build();
             HtmlRenderer renderer = HtmlRenderer.builder(options)
-                    .attributeProviderFactory(new IndependentAttributeProviderFactory() {
-                        @Override
-                        public AttributeProvider create(NodeRendererContext nodeRendererContext) {
-                            File rootDir = file.getParentFile().getParentFile();
-                            return new CustomAttributeProvider(rootDir);
-                        }
-                    }).build();
+//                    .nodeRendererFactory()
+//                    .linkResolverFactory()
+//                    .htmlIdGeneratorFactory()
+//                    .attributeProviderFactory(new IndependentAttributeProviderFactory() {
+//                        @Override
+//                        public AttributeProvider create(NodeRendererContext nodeRendererContext) {
+//                            File rootDir = file.getParentFile().getParentFile();
+//                            return new CustomAttributeProvider(rootDir);
+//                        }
+//                    })
+                    .build();
 
             Node document = parser.parse(content);
             String html = renderer.render(document);
@@ -99,12 +87,52 @@ public class ArticleServlet extends HttpServlet {
         }
     }
 
+    private String readFile(final File file) {
+        // 从文件中读取markdown内容
+        List<String> list = new LinkedList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+
+        // 兼容hexo生成的文章
+        if (isFromHexo(list)) {
+            int count = 6;
+            while (count-- > 0) list.remove(0);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : list) {
+            sb.append(str).append("\n");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
+    }
+
+    private boolean isFromHexo(List<String> list) {
+        if (list.size() < 6) return false;
+        if (!list.get(0).equals("---")) return false;
+        if (!list.get(1).startsWith("title:")) return false;
+        if (!list.get(2).startsWith("date:")) return false;
+        if (!list.get(3).startsWith("tags:")) return false;
+        if (!list.get(4).startsWith("categories:") && !list.get(4).equals("---")) return false;
+        if (list.get(4).startsWith("categories:") && !list.get(5).equals("---")) return false;
+
+        return true;
+    }
+
     /**
      * 处理标签的属性
      */
     static class CustomAttributeProvider implements AttributeProvider {
         private File rootDir;
-        
+
         public CustomAttributeProvider(File rootDir) {
             this.rootDir = rootDir;
         }
@@ -114,9 +142,9 @@ public class ArticleServlet extends HttpServlet {
             if (false == node instanceof Image || !"LINK".equals(attributablePart.getName())) {
                 return;
             }
-            
-            if (true)return;
-            
+
+            if (true) return;
+
             File file = new File(rootDir, attributes.getValue("src"));
             attributes.replaceValue("src", file.getAbsolutePath());
         }
